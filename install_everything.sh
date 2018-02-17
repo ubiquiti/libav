c47c01cce5ec1fbdea816bc3fcaca0c55be6ae1a
Gavriloaie Eugen-Andrei <andrei@ubnt.com>
2018-02-17 20:02:50 +0200

    -- small script to install it locally

---
+++ install_everything.sh
@@ -0,0 +1,17 @@
+
+#!/bin/sh
+
+set -e
+
+DEST_FOLDER=${LOCAL_PREFIX}/libav_install
+
+./configure \
+	--prefix=${DEST_FOLDER} \
+	--extra-cflags="-I${LOCAL_PREFIX}/include -I${DEST_FOLDER}/include" \
+	--extra-ldflags="-L${LOCAL_PREFIX}/lib -L${DEST_FOLDER}/lib" \
+	--enable-gpl \
+	--enable-libx264 \
+
+
+make -j32 install
+
